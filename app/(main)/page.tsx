482df9d7c723e2223d8051d073bd1ae4bdeb1b64
abhishek-01k <abhishek@push.org>
2024-07-07 18:56:45 +0530

    Added Fxtl point system to dashboard

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -6,23 +6,21 @@ import { Folder, MessageCircle, Newspaper, User } from 'lucide-react';
 import { createThirdwebClient } from 'thirdweb';
 import { fraxMainnet } from '@/config/Chains';
 import { FraxtalPointSystemABI } from '@/config/ContractABI';
-import { useContractEvents } from 'thirdweb/react';
+import { useActiveAccount, useActiveWallet, useActiveWalletConnectionStatus, useConnect, useContractEvents, useReadContract, useWalletInfo } from 'thirdweb/react';
 import { createPublicClient, getContract, http } from 'viem';
 import { fraxtal } from 'viem/chains'
+import { useEffect, useState } from 'react';
 export default function Home() {
+
+  const activeAccount = useActiveAccount();
+
+  const status = useActiveWalletConnectionStatus();
+
+  const [fxtlPoints, setFxtlPoints] = useState(0);
 
   const client = createThirdwebClient({ clientId: '55691db011a0294906c77ced2fea9718' });
 
-  const getContractInstance = async () => {
-
-    // const contract = getContract({
-    //   client,
-    //   chain: fraxMainnet,
-    //   address: "0xab4b7c5c9a7c8ebb97877085a6c3550ad4ed3f97",
-    // });
-
-    // console.log("Contract instance >>>", contract);
-
+  const getFxtlPoints = async () => {
     const publicClient = createPublicClient({
       chain: fraxtal,
       transport: http(),
@@ -36,17 +34,33 @@ export default function Home() {
 
     console.log("Contract instance >>>", contract);
 
+    // const points = await contract.read.balanceOf(['0x59cfcd384746ec3035299d90782be065e466800b'])
+    const points = await contract.read.balanceOf([activeAccount?.address])
+    const intVal = Number(points);
+    setFxtlPoints(intVal);
   }
+
+  useEffect(() => {
+    if (status == 'connected') {
+      getFxtlPoints();
+    }
+
+    if (status == 'disconnected') {
+      setFxtlPoints(0)
+    }
+
+  }, [status])
+
+
 
   return (
     <>
       <h1 className='text-center text-blue-400'>FXTL Points Dashboard</h1>
 
-      <div onClick={getContractInstance}> Contract Instance </div>
       <div className='flex flex-col md:flex-row justify-between gap-5 mb-5'>
         <DashboardCard
           title='FXTLPoints'
-          count={100}
+          count={fxtlPoints}
           icon={<Newspaper className='text-slate-500' size={72} />}
         />
         <DashboardCard
@@ -58,11 +72,6 @@ export default function Home() {
           title='Users'
           count={750}
           icon={<User className='text-slate-500' size={72} />}
-        />
-        <DashboardCard
-          title='Comments'
-          count={1200}
-          icon={<MessageCircle className='text-slate-500' size={72} />}
         />
       </div>
       <AnalyticsChart />
